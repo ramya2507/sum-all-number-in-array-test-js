a0a3e79c668512e01609fce629a810fe117d6725
Ramya Adimoolam <ramyaa1431@gmail.com>
2020-11-27 06:32:27 +0000

    Added recursive function to find sum of all elements of array

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let total = 0;
+  for (let i = 0; i < array.length ; i++) {
+    if(Array.isArray(array[i])){
+      total += sumItems(array[i]);
+    } else {
+      total += array[i];
+    }
+  }
+  return total;
 }
-
 module.exports = sumItems;
